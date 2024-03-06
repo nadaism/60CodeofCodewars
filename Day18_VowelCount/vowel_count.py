c10271eae59292c5e31f193bede915594c756ea1
nadaism <putudesianada@gmail.com>
2024-03-06 20:33:00 +0700

    Hari 18

---
+++ Day18_VowelCount/vowel_count.py
@@ -0,0 +1,15 @@
+"""
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+"""
+
+def get_count(sentence):
+    vowel = 'aeiuo'
+    count = 0
+    for char in sentence:
+        if char in vowel:
+            count += 1
+    return count
