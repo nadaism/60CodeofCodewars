2f6c16282e10119bf2202b55cedc085bcf67b545
nadaism <putudesianada@gmail.com>
2024-03-06 20:59:33 +0700

    Hari 20

---
+++ Day20_StringEndWith/string_end_with.py
@@ -0,0 +1,6 @@
+"""
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+"""
+def solution(text, ending):
+    # your code here...
+    return text.endswith(ending)
