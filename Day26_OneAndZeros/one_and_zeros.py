81f24f639a60111488e9945769f70fd819ac48e5
nadaism <putudesianada@gmail.com>
2024-03-18 22:22:13 +0700

    Hari 26

---
+++ Day26_OneAndZeros/one_and_zeros.py
@@ -0,0 +1,10 @@
+"""
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+"""
+
+def binary_array_to_number(arr):
+  # your code
+    binary_string = ''.join(map(str, arr))
+    return int(binary_string, 2)
