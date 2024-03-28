ec87adbfd18345b24ce8ede9d2fabc8757038976
nadaism <putudesianada@gmail.com>
2024-03-28 21:14:04 +0700

    Hari 53

---
+++ Day53_FindTheStrayNumber/find_the_stray_number.py
@@ -0,0 +1,13 @@
+"""
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+"""
+def stray(arr):
+    hasil = 0
+    for num in arr:
+        hasil ^= num
+    return hasil
