0283a1bb4f9d402cf69793ca078c8f1a88c8744d
nadaism <putudesianada@gmail.com>
2024-03-28 20:32:35 +0700

    Hari 51

---
+++ Day51_RemoveAnchorFromURL/remove_anchor_from_url.py
@@ -0,0 +1,7 @@
+"""
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+"""
+
+def remove_url_anchor(url):
+    # TODO: complete
+    return url.split("#")[0]
